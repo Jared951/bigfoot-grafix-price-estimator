eb732a3b0bdd8d712109dcf1f5fe7120d46259ca
Jared Sewak <jaredsewak@gmail.com>
2024-01-25 20:30:21 -0700

    added remaining sign material options to cascading dropdown lists

--- client/index.js
+++ client/index.js
@@ -4,17 +4,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function signOptions() {
     const signTypeSelection = document.getElementById("signTypeSelection");
-    const backlitMaterialSelection = document.getElementById("backlitMaterialSelection");
+    const materialSelection = document.getElementById("materialSelection");
 
     // Clear existing options
-    backlitMaterialSelection.innerHTML = "<option value='non'>Select</option>";
+    materialSelection.innerHTML = "<option value='non'>Select</option>";
 
     // Get selected Sign Material Type
     const selectedSignMaterialType = signTypeSelection.value;
 
     // Populate Sign Select Material based on the selected material type
     if (selectedSignMaterialType === "backlit") {
-        addOption(backlitMaterialSelection, "10.50_per_square_foot", "10.50 per square foot");
+        addOption(materialSelection, "10.50_per_square_foot", "10.50 per square foot");
+    } else if (selectedSignMaterialType === "acrylic") {
+        addOption(materialSelection, "acrylic_half_clear", "Acrylic 1/2 Clear: $23.25 per square foot");
+        addOption(materialSelection, "acrylic_quarter_clear", "Acrylic 1/4 Clear: $16.75 per square foot");
+        addOption(materialSelection, "acrylic_eighth_clear", "Acrylic 1/8 Clear: $12.35 per square foot");
+    } else if (selectedSignMaterialType === "windowDecal") {
+        addOption(materialSelection, "window_perf_50_50", "Window Perf 50/50: $6.25 per square foot");
+        addOption(materialSelection, "window_perf_65_35", "Window Perf 65/35: $6.50 per square foot");
     }
 }
 
